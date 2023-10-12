0ac85a090a30994801dee3adeb9c263bc8cddfd6
Jihadned <jjadned@gmail.com>
2023-10-12 16:52:53 -0400

    A10 Final

--- assignments/a10/script.js
+++ assignments/a10/script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    MaiAppend();
+    showToy();
 }
 
 const toys = [];
@@ -8,7 +8,7 @@ const showToy = () => {
     const toyList = document.getElementById("toy-list");
     
     // Lego Bugatti: Matthew
-    toys.push(new Toy("Lego Bugatti", "$385.99", "16+", 4.8, "legocar.jpeg"));
+    toys.push(new Toy("Lego Bugatti", "$385.99", "16+", 4.8, "legocar.png"));
     // Forkknife Bazooka Nerfgun: Daniel
     toys.push(new Toy("Fortnite Bazooka Nerfgun", "$53.18", "8+", 4.6, "forkknife.jpg"));
     // Beyblade Pegasus: Willyum
@@ -16,24 +16,15 @@ const showToy = () => {
     // Gameboy: JJ
     toys.push(new Toy("Gameboy Color", "$93.93", "8+", 4.4, "gameboy.jpg"));
     // Harrypotter Vibrating Broomstick: Matthew... again
-    toys.push(new Toy("Harry Potter Nimbus 2000 Vibrating Broomstick", "$41.41", "8+", 4.1, "harrypotter.jpg"));
+    toys.push(new Toy("Harry Potter Nimbus 2000 Vibrating Broomstick", "$41.41", "8+", 4.1, "harrypotter.png"));
     // Little Pet Shop: Lily
-    toys.push(new Toy("Littlest Pet Shop", "$27.99", "2+", 4.0, "petshop.jpg"));
+    toys.push(new Toy("Littlest Pet Shop", "$27.99", "2+", 4.0, "petshop.png"));
 
     toys.forEach(toy => {
         toyList.append(toy.pica);
-        toyList.append(toy.item);
     });
 
 };
-
-const MaiAppend = () => {
-    const Main = document.getElementById("main");
-    Main.style.position = "relative";
-    Main.style.float = "left";
-    Main.style.margin = "5px";
-    Main.append(showToy());
-}
 
 class Toy {
     constructor(name, price, age, rating, image) {
@@ -43,38 +34,39 @@ class Toy {
         this.rating = rating;
         this.image = image;
     }
-    
-
-    get item() {
-        const div = document.createElement("div");
-        div.classList.add("toyz");
-
-        const h3 = document.createElement("h4");
-        h3.innerHTML = this.name;
-        div.append(h3);
-
-        const ul = document.createElement("ul");
-        div.append(ul);
-        ul.append(this.listItem(this.price));
-        ul.append(this.listItem("Ages: "+ this.age));
-        ul.append(this.listItem(this.rating + " out of 5 stars"));
-
-        div.style.position = "absolute";
-        div.style.top = "5px";
-        div.style.left = "5px";
-
-        return div;
-    }
 
     get pica() {
         const pic = document.createElement("div");
         pic.classList.add("pictura");
         pic.append(this.picture(this.image));
+        
+
+        const h4 = document.createElement("h4");
+        h4.innerHTML = this.name;
+        pic.append(h4);
+
+        const ul = document.createElement("ul");
+        pic.append(ul);
+        ul.append(this.listItem("Price: " + this.price));
+        ul.append(this.listItem("Ages: "+ this.age));
+        ul.append(this.listItem(this.rating + " out of 5 stars"));
+
+        h4.style.position = "relative";
+        h4.style.top = "-200px";
+        h4.style.left = "125px";
+        h4.style.width = "50%";
+
+        ul.style.position = "relative";
+        ul.style.top = "-200px";
+        ul.style.left = "-25px";
+        ul.style.listStyleType = "none";
+        
         return pic;
     }
 
     picture(info) {
         let png = document.createElement("img");
+        png.style.position = "relative";
         png.src = './toyimages/' + info;
         return png;
     }
