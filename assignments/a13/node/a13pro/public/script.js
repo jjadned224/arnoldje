2be0baf60db320da10e9caa1a5bf8d47a43dc84c
Jihadned <jjadned@gmail.com>
2023-11-05 14:29:49 -0500

    A13 V1

---
+++ assignments/a13/node/a13pro/public/script.js
@@ -0,0 +1,41 @@
+const showGames = async () => {
+    let response = await fetch("http://localhost:3000/api/games");
+    let gameJSON = await response.json();
+    let gameDiv = document.getElementById("game-list");
+
+    gameJSON.forEach(game => {
+        let section = document.createElement("section");
+        gameDiv.append(section);
+
+        let h3 = document.createElement("h3");
+        section.append(h3);
+        h3.innerHTML = game.name;
+
+        let sect2 = document.createElement("section");
+        sect2.classList.add("rower");
+
+        let img = document.createElement("img");
+        img.src = game.img;
+        sect2.append(img);
+
+        let ul = document.createElement("ul");
+    
+        sect2.append(ul);
+        ul.append(getLi(`Developer: ${game.dev}`));
+        ul.append(getLi(`Released: ${game.release}`));
+        ul.append(getLi(`My Personal Playtime: ${game.playtime}`));
+        ul.append(getLi(`Number of Achievements: ${game.achiv}`));
+        ul.append(getLi(`Achievements Under 10% Completion: ${game.under10}`));
+        section.appendChild(sect2);
+    });
+};
+
+const getLi = (data) => {
+    const li = document.createElement("li");
+    li.textContent = data;
+    return li;
+  };
+
+window.onload = () => {
+    showGames();
+};
