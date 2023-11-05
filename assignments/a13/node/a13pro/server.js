bd16b8b439ad2f09e87751c7ccf138b33f659267
Jihadned <jjadned@gmail.com>
2023-11-05 15:08:39 -0500

    A13 V2

--- assignments/a13/node/a13pro/server.js
+++ assignments/a13/node/a13pro/server.js
@@ -16,7 +16,7 @@ app.get("/api/games", (req, res) => {
         achiv: "63 Achievements",
         under10: ["Focus"," Happy Couple"," Soul & Shade"," Banishment"," Neglect"," Keen Hunter"," True Hunter"," Passing of the Age",
         " Pure Completion"," Speedrun 1"," Speedrun 2"," Speed Completion"," Embrace the Void"," Steel Soul"," Steel Heart"],
-        img: "./game-img/hk.png"
+        img: "./game-img/HK.png"
     }
     games[1] = {
         name: "ARMORED CORE VI FIRES OF RUBICON",
