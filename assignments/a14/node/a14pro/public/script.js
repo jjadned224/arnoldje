a8b82a10d2a0d3f6bc50a172b2c32fcad09ad841
Jihadned <jjadned@gmail.com>
2023-11-12 18:17:04 -0500

    Half working A14

--- assignments/a14/node/a14pro/public/script.js
+++ assignments/a14/node/a14pro/public/script.js
@@ -1,42 +1,104 @@
-const showGames = async () => {
-
-    let response;
-    let gameJSON;
-    try {
-        response = await fetch("./api/games");
-        gameJSON = await response.json();
-    } catch(error) {
-        console.log("error getting Json");
+let count = 0;
+let songDiv;
+let clearDiv;
+let section;
+let sect2;
+const showSongs = async () => {
+      
+    if (count == 1) {
+        clearSongs();
     }
-let gameDiv = document.getElementById("game-list");
+    let songs = await getSong();
+    console.log(songs);
 
-gameJSON.forEach(game => {
-    let section = document.createElement("section");
-    gameDiv.append(section);
+songDiv = document.getElementById("song-list");
+
+
+let songJSON = await getSong();
+console.log(songJSON);
+songJSON.forEach(song => {
+
+    section = document.createElement("section");
+    songDiv.append(section);
+
+    
 
     let h3 = document.createElement("h3");
     section.append(h3);
-    h3.innerHTML = game.name;
+    h3.innerHTML = song.name;
 
-    let sect2 = document.createElement("section");
+    
+
+    sect2 = document.createElement("section");
     sect2.classList.add("rower");
 
+    const a = document.createElement("a");
+    a.href = "#rower";
+    a.style.height = '300px';
+
     let img = document.createElement("img");
-    img.src = game.img;
-    sect2.append(img);
+    img.src = song.img;
+    a.appendChild(img);
+    sect2.append(a);
+    let embeded = document.createElement("iframe");
+    let shower = 0;
+    
+
 
     let ul = document.createElement("ul");
 
     sect2.append(ul);
-    ul.append(getLi(`Developer: ${game.dev}`));
-    ul.append(getLi(`Released: ${game.release}`));
-    ul.append(getLi(`My Personal Playtime: ${game.playtime}`));
-    ul.append(getLi(`Number of Achievements: ${game.achiv}`));
-    ul.append(getLi(`Achievements Under 10% Completion: ${game.under10}`));
-    section.appendChild(sect2);
+    ul.append(getLi(`Artist: ${song.Artist}`));
+    ul.append(getLi(`Released: ${song.release}`));
+    ul.append(getLi(`Runtime: ${song.runtime}`));
+    ul.append(getLi(`Genres: ${song.genres}`));
+    ul.append(getLi(`Recommended By: ${song.recby}`));
+
+    embeded.style = song.spotifylink[0];
+    embeded.src = song.spotifylink[1];
+    embeded.width = song.spotifylink[2];
+    embeded.height = song.spotifylink[3];
+    embeded.frameBorder = song.spotifylink[4];
+    embeded.allowFullscreen = song.spotifylink[5];
+    embeded.allow = song.spotifylink[6];
+    embeded.loading = song.spotifylink[7];
+    embeded.style.display = "none";
+    embeded.style.marginRight = "5%";
+    embeded.style.marginLeft = "5%";
+    embeded.style.position = "relative";
+
+    a.onclick = () => {
+        if (shower == 0) {
+            ul.style.display = "none";
+            embeded.style.display = "inline";
+            shower = 1;
+        } else {
+            ul.style.display = "inline";
+            embeded.style.display = "none";
+            shower = 0;
+        }
+    }
+    sect2.append(embeded)
+        
+    section.append(sect2);
     values++;
+
+    
 });
+count = 1;
 };
+
+const clearSongs = () => {
+    songDiv.removeChild(sect2);
+}
+
+const getSong = async () => {
+    try {
+        return (await fetch("api/songs")).json();
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 const getLi = (data) => {
 const li = document.createElement("li");
@@ -47,5 +109,77 @@ return li;
 let values = 0;
 
 window.onload = () => {
-showGames();
+showSongs();
+
+const butt1 = document.getElementById("form"); 
+const butt2 = document.getElementById("list");
+butt1.onclick = showform;
+butt2.onclick = showlist;
+const butt3 = document.getElementById("submit");
+butt3.onclick = addForm;
+
 };
+
+const resetForm = () => {
+    const form = document.getElementById("inputform");
+    form.reset();
+    form._id = "-1";
+}
+
+const addForm = async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("inputform");
+    const formData = new FormData(form);
+    let response; 
+    if (form._id.value == -1) {
+        formData.delete("_id");
+        formData.delete("img");
+        formData.append("Dataz", getDatas());
+
+        console.log(...formData);
+
+        response = await fetch ("/api/songs", {
+            method: "POST",
+            body: formData,
+        });
+    } 
+    if(response.status != 200) {
+        console.log("error");
+    }
+    
+
+    console.log(response);
+    console.log(form._id);
+    
+    resetForm();
+    showSongs();
+}
+
+const showform = () => {
+    let list = document.getElementById("song-list");
+    list.classList.add("hide");
+    let formDiv = document.getElementById("formdiv");
+    formDiv.classList.remove("hide");
+    document.getElementById("form").classList.add("hide");
+    document.getElementById("list").classList.remove("hide");
+}
+
+const showlist = () => {
+    let list = document.getElementById("song-list");
+    list.classList.remove("hide");
+    let formDiv = document.getElementById("formdiv");
+    formDiv.classList.add("hide");
+    document.getElementById("form").classList.remove("hide");
+    document.getElementById("list").classList.add("hide");
+}
+
+const getDatas = () => {
+    const inputs = document.querySelectorAll("#datz input");
+    let Dataz = [];
+
+    inputs.forEach((input) => {
+        Dataz.push(input.value);
+    });
+
+    return Dataz;
+}
--- assignments/a14/node/a14pro/server.js
+++ assignments/a14/node/a14pro/server.js
@@ -1,72 +1,149 @@
 const express = require("express");
 const app = express();
+const Joi = require("joi");
+const multer = require("multer");
 app.use(express.static('public'));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+
+const upload = multer({dest: __dirname + "/public/songimg"});
 
 app.get('/',(req, res)=>{
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/games", (req, res) => {
-    const games = [];
-    games[0] = {
-        name: "Hollow Knight",
-        dev: "Team Cherry",
-        release: "Feb 24th, 2017",
-        playtime: "54 hours",
-        achiv: "63 Achievements",
-        under10: ["Focus"," Happy Couple"," Soul & Shade"," Banishment"," Neglect"," Keen Hunter"," True Hunter"," Passing of the Age",
-        " Pure Completion"," Speedrun 1"," Speedrun 2"," Speed Completion"," Embrace the Void"," Steel Soul"," Steel Heart"],
-        img: "./game-img/HK.png"
+app.get("/api/songs", (req, res) => {
+    let songs = [
+    {
+        _id: 1,
+        name: "Stranger Things Have Happened",
+        Artist: ["Foo Fighters"],
+        release: "Sep 25th, 2007",
+        runtime: "5:21",
+        genres: ["Alternative Rock", " Post-Grunge", " Hard Rock"],
+        recby: ["Jaden"],
+        img: "./songimg/ESPG.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/7zaZlzl0XhthNwH3GQcyZ0?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 2,
+        name: "This is War",
+        Artist: ["Alter Bridge"],
+        release: "Oct 14th, 2022",
+        runtime: "4:04",
+        genres: ["Alternative Rock", " Progressive Metal",],
+        recby: ["Jaden"],
+        img: "./songimg/P&Ks.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/2e8F1M3b7dI2GDUDZh6c9l?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 3,
+        name: "I'm Not Alright",
+        Artist: ["Shinedown"],
+        release: "Mar 27th, 2012",
+        runtime: "3:07",
+        genres: ["Hard-Rock", " Post-Grunge",],
+        recby: ["Jaden"],
+        img: "./songimg/Am.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/558KeqwhMiNafy3Dy4kan5?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 4,
+        name: "Bleed American",
+        Artist: ["Jimmy Eat World"],
+        release: "July 24th, 2001",
+        runtime: "3:01",
+        genres: ["Emo", " Alternative Rock", " Pop-punk", " Power pop"],
+        recby: ["Jaden"],
+        img: "./songimg/BA.jpeg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/61XspFITuKmAlYdQacNCbF?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 5,
+        name: "When the Fire and the Rose Were One",
+        Artist: ["Wilderun"],
+        release: "Nov 1st, 2019",
+        runtime: "11:51",
+        genres: ["Metal", " Progressive Metal", " Folk", " Orchestral"],
+        recby: ["Jaden"],
+        img: "./songimg/Veil.png",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/5AEWvbrAUhj7t5tSkkW1ZM?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 6,
+        name: "Suzanne",
+        Artist: ["Creeper"],
+        release: "Oct 3rd, 2016",
+        runtime: "2:40",
+        genres: ["Pop Punk", " Punk Rock",],
+        recby: ["Jaden"],
+        img: "./songimg/Suz.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/7fa1JCl90Uamf9SxBWNBTP?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 7,
+        name: "The Man Who Sold The World - Live",
+        Artist: ["Nirvana"],
+        release: "Nov 18th, 1993",
+        runtime: "4:21",
+        genres: ["Grunge", " Alternative", " Indie", " Blues"],
+        recby: ["Jaden"],
+        img: "./songimg/TMWSTW.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/15VRO9CQwMpbqUYA7e6Hwg?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
+    },
+    {
+        _id: 8,
+        name: "A Song for the Birds",
+        Artist: ["Eisley"],
+        release: "February 17th, 2017",
+        runtime: "4:01",
+        genres: ["Alternative", " Indie", " Rock"],
+        recby: ["Jaden"],
+        img: "./songimg/Dreaming.jpg",
+        spotifylink: ['border-radius:20px', 'https://open.spotify.com/embed/track/3xKKKzMj0uwE8bqdJWHwZP?utm_source=generator', '100%', '152px', '0', '', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture', 'lazy'],
     }
-    games[1] = {
-        name: "ARMORED CORE VI FIRES OF RUBICON",
-        dev: "FromSoftware Inc.",
-        release: "Aug 24th, 2023",
-        playtime: "56.7 hours",
-        achiv: "30 Achievements",
-        under10: ["The Perfect Mercenary"," Armored Core"],
-        img: "./game-img/ac6.avif"
-    }
-    games[2] = {
-        name: "ELDEN RING",
-        dev: "FromSoftware Inc.",
-        release: "Feb 24th, 2022",
-        playtime: "237.9 hours",
-        achiv: "42 Achievements",
-        under10: ["Elden Ring"],
-        img: "./game-img/er.jpg"
-    }
-    games[3] = {
-        name: "Cuphead",
-        dev: "Studio MDHR Entertainment Inc.",
-        release: "Sep 29th, 2017",
-        playtime: "16.8 hours",
-        achiv: "42 Achievements",
-        under10: ["Butter-and-Egg Man"," Alive and Kicking"," Sheriff"," The Lastest Sensation"," Put On a Show"," High Roller"," King"," Rolling Sixes"
-        ," A Vacation in the Wilds"," Checkmate"," Compliments to the Chef"," Boss"," A Horrible Night To Have A Curse"," The Golden Touch"
-        ," Mayor"," Pacifist"," Decadent"," Bravo Zulu P-26"," Beat The Devil At His Own Game"," Hearty"," Paladin"," A King's Admiration"," The High Hat"," Ranger"," Cooked to Perfection"],
-        img: "./game-img/cuphead.png"
-    }
-    games[4] = {
-        name: "ARK: Survival Evolved",
-        dev: "Studio Wildcard",
-        release: "Aug 27th, 2017",
-        playtime: "987.7 hours",
-        achiv: "32 Achievements",
-        under10: ["Survivor Evolved"," Highest Peak"," Experience Explorer"," Alpha Ascension"," Cure-All"," Adventurous Explorer"," Gamma Ascension"," Studious Explorer"," Artifact Archaeologist"," Vetern Explorer"," Veteran Paleontologist"," Adept Explorer"," Professional Explorer"," Master Explorer"," Survivor of The Center"," Perfect Explorer"," Beta Ascension"," Master Zoologist"],
-        img: "./game-img/ark.webp"
-    }
-    games[5] = {
-        name: "The Stanley Parable",
-        dev: "Galactic Cafe",
-        release: "Oct 17th, 2013",
-        playtime: "30.2 hours",
-        achiv: "10 Achievements",
-        under10: ["Speed run"," Unachievable"," Commitment"],
-        img: "./game-img/Stanley_parable_cover.jpg"
-    }
-    res.json(games);
+];
+    res.json(songs);
 });
+
+app.post("api/songs", upload.single("img"), (req, res)=> {
+    const result = validateSong(req.body);
+
+    if (result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const song = {
+        _id: songs.length+1,
+        name: req.body.name,
+        Artist: req.body.Artist.split(","),
+        release: req.body.release,
+        runtime: req.body.runtime,
+        genres: req.body.genres.split(","),
+        recby: req.body.recby.split(","),
+        img: req.body.img,
+        spotifylink: req.body.spotifylink,
+    }
+
+    songs.push(song);
+    res.send(song);
+});
+
+const validateSong = () => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        name: Joi.string().min(3).required(),
+        Artist: Joi.string().min(3).require(),
+        release: Joi.string().min(3).require(),
+        runtime: Joi.string().min(3).require(),
+        genres: Joi.string().min(3).require(),
+        recby: Joi.string().min(3).require(),
+        spotifylink: Joi.link().min(3),
+    });
+    return schema.validate(songs);
+}
 
 app.listen(3000, () => {
     console.log("Working");
